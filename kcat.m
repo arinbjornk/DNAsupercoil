66fe4b839a46b4c8d1bcaf5804e898bf0cd8109b
Arinbjorn Kolbeinsson <ak711@vm-shell1.doc.ic.ac.uk>
2015-03-20 11:45:48 +0000

    Implement Euler method

--- kcat.m
+++ kcat.m
@@ -2,7 +2,7 @@ function [ out ] = kcat( sigma, max, TL  )
 %UNTITLED2 Summary of this function goes here
 %   Detailed explanation goes here
 
-out = (max/TL)/(abs(sigma+0.65)+1);
+out = (max/TL)/(abs(sigma+0.65)+1)
 
 end
 
