73f7ceccb3107e7cb2756f8e845dbad79e7c1b60
Arinbjorn <arinbjorn@gmail.com>
2015-05-11 17:59:31 +0100

    Does not explode

--- tandem_debug.m
+++ tandem_debug.m
@@ -13,15 +13,15 @@ end
 %plot(sigma);
 
 %% Tandem
-delta = 0.000001;
-T = 10000;
+delta = 1;
+T = 100;
 h0 = 10.5;
 tau = 0.25;
 gamma = 0.5;
 sigma_0 = -0.65;
 delta_LN = 5;
 delta_m = 0.05;
-omega = 7.85*10^11;
+omega = 10^5;%7.85*10^11;
 TL_S = 141;
 TL_G = 50;
 PL_S = 40;
@@ -33,20 +33,20 @@ k_w = 1;
 k_l = 0.02;
 k_r = 1;
 
-R_tot = 1000;
-PLac_tot = 11;
-PTet_tot = 11;
-
 sigma_tS = -0.65;
 sigma_tG = -0.65;
 sigma_pS = -0.65;
 sigma_pG = -0.65;
 mS = 0;
 MG = 0;
-EC_S = 0;
-EC_G = 0;
+EC_S = 0.1*10^-9;
+EC_G = 0.1*10^-9;
 k_seq = 0.1;
-ECGECS = 0;
+ECGECS = 0.1*10^-9;
+
+R_tot = 10^-6 - EC_S + EC_G + ECGECS;
+PLac_tot = 11*10^-9;% EC_S + ECGECS;
+PTet_tot = 11*10^-9;% EC_G + ECGECS;
 
 for i=1:T
     
@@ -57,7 +57,7 @@ if(sigma_pS>sigma_0)Bg_pS = 1;
 else Bg_pS = 0;
 end
 
-if(sigma_tS<sigma_0)Bt_tS = 1;
+if(sigma_tS<=sigma_0)Bt_tS = 1;
 else Bt_tS = 0;
 end
 if(sigma_tS>sigma_0)Bg_tS = 1;
@@ -78,35 +78,42 @@ if(sigma_tG>sigma_0)Bg_tG = 1;
 else Bg_tG = 0;
 end
 
+if(sigma_tS>sigma_0)BtS = 1;
+else BtS = 0;
+end
+
 %R_tot(i) = R + EC_S(i) + EC_G(i) + ECGECS(i);
-PLac = PLac_tot - EC_S(i) + ECGECS(i);
-PTet = PTet_tot - EC_G(i) + ECGECS(i);
+PLac(i) = PLac_tot - EC_S(i) - ECGECS(i);
+PTet(i) = PTet_tot - EC_G(i) - ECGECS(i);
 
-sigma_tS(i+1) =  sigma_tS(i) + delta*(-(omega/2)*(kcat(sigma_tS(i), kcat_max, TL_S)*EC_S(i) ) + (h0/TL_S)*(tau*Bt_tS-gamma*Bg_tS) );
-sigma_pG(i+1) = sigma_pG(i) + delta*(-(omega/2)*(kf(sigma_pG(i), kf_max)*PTet*(R_tot - (EC_S(i) + EC_G(i) + ECGECS(i)))) + (h0/PL_G)*(tau*Bt_pG-gamma*Bg_pG));
+sigma_tS(i+1) =  sigma_tS(i) + (delta)*(-(omega/2)*(kcat(sigma_tS(i), kcat_max, TL_S)*EC_S(i) ) + (h0/TL_S)*(tau*Bt_tS-gamma*Bg_tS) );
+sigma_pG(i+1) = sigma_pG(i) + delta*(-(omega/2)*(kf(sigma_pG(i), kf_max)*PTet(i)*(R_tot - (EC_S(i) + EC_G(i) + ECGECS(i)))) + (h0/PL_G)*(tau*Bt_pG-gamma*Bg_pG));
 
-sigma_pS(i+1) = sigma_pS(i) + delta*((omega/2)*(kcat(sigma_pG(i), kcat_max, TL_G)*EC_G(i)*(TL_G/(2*(PL_S+NS))) - kf(sigma_pS(i), kf_max)*PLac*(R_tot - (EC_S(i) + EC_G(i) + ECGECS(i)))-kcat(sigma_tS(i), kcat_max, TL_G)*EC_S(i)*(TL_G/(PL_S+NS))) + (h0/PL_S)*(tau*Bt_pS-gamma*Bg_pS));
-sigma_tG(i+1) = sigma_tG(i) + delta*(-(omega/2)*(kcat(sigma_tS(i), kcat_max, TL_S)*EC_G(i) + kcat(sigma_tG(i), kcat_max, TL_G)*EC_S(i)*(TL_S/(PL_S+NS+TL_G+TL_S)) + kf(sigma_pS(i), kf_max)*PLac*(R_tot - (EC_S(i) + EC_G(i) + ECGECS(i)))*(PL_S/(2*(TL_G+NS)))) + (h0/TL_S)*(tau*Bt_tG-gamma*Bg_tG));
+sigma_pS(i+1) = sigma_pS(i) + delta*((omega/2)*(kcat(sigma_pG(i), kcat_max, TL_G)*EC_G(i)*(TL_G/(2*(PL_S+NS))) - kf(sigma_pS(i), kf_max)*PLac(i)*(R_tot - (EC_S(i) + EC_G(i) + ECGECS(i)))-kcat(sigma_tS(i), kcat_max, TL_G)*EC_S(i)*(TL_G/(PL_S+NS))) + (h0/PL_S)*(tau*Bt_pS-gamma*Bg_pS));
+sigma_tG(i+1) = sigma_tG(i) + delta*(-(omega/2)*(kcat(sigma_tS(i), kcat_max, TL_S)*EC_G(i) + kcat(sigma_tG(i), kcat_max, TL_G)*EC_S(i)*(TL_S/(PL_S+NS+TL_G+TL_S)) + kf(sigma_pS(i), kf_max)*PLac(i)*(R_tot - (EC_S(i) + EC_G(i) + ECGECS(i)))*(PL_S/(2*(TL_G+NS)))) + (h0/TL_S)*(tau*Bt_tG-gamma*Bg_tG));
 
-mS(i+1) = mS(i) + delta*(kcat(sigma_tS(i), kcat_max, TL_S)*EC_S(i)+k_w*ECGECS(i)-delta_m*mS(i));
+mS(i+1) = mS(i) + (delta)*(kcat(sigma_tS(i), kcat_max, TL_S)*EC_S(i)+k_w*ECGECS(i)-delta_m*mS(i));
 MG(i+1) = MG(i) + delta*(kcat(sigma_tG(i), kcat_max, TL_S)*EC_G(i)+k_w*ECGECS(i)-delta_m*MG(i));
-EC_S(i+1) = EC_S(i) + delta*(kf(sigma_pS(i), kf_max)*(R_tot - (EC_S(i) + EC_G(i) + ECGECS(i)))*PLac-(k_r+kcat(sigma_tS(i), kcat_max, TL_S))*EC_S(i));
-EC_G(i+1) = EC_G(i) + delta*(kf(sigma_pG(i), kf_max)*(R_tot - (EC_S(i) + EC_G(i) + ECGECS(i)))*PTet-(k_r+kcat(sigma_tG(i), kcat_max, TL_G)+kseq(sigma_tG)+k_l)*EC_G(i));
-ECGECS(i+1) = ECGECS(i) + delta*(k_l*EC_G(i) - k_w*ECGECS(i));
+EC_S(i+1) = EC_S(i) ;%+ (delta/1)*(kf(sigma_pS(i), kf_max)*(R_tot - (EC_S(i) + EC_G(i) + ECGECS(i)))*PLac(i)-(k_r+kcat(sigma_tS(i), kcat_max, TL_S))*EC_S(i));
+EC_G(i+1) = EC_G(i) ;%+ (delta/1)*(kf(sigma_pG(i), kf_max)*(R_tot - (EC_S(i) + EC_G(i) + ECGECS(i)))*PTet(i)-(k_r+kcat(sigma_tG(i), kcat_max, TL_G)+kseq(sigma_tG)+k_l)*EC_G(i));
+ECGECS(i+1) = ECGECS(i) ;%+ (delta/1)*(k_l*EC_G(i) - k_w*ECGECS(i));
 
 X(i) = EC_S(i) + EC_G(i) + ECGECS(i);
 Y(i) = kcat(sigma_tS(i), kcat_max, TL_S);
+Z(i) = (h0/TL_S)*(tau*Bt_tS-gamma*Bg_tS);
 
 end
 figure;
 hold on;
-% plot(sigma_pS);
-plot(EC_S);
+%plot(Y);
+%plot(mS);
 %plot(MG);
-% legend('show')
-%plot(sigma_pG);
-%plot(sigma_tG);
+ 
+plot(sigma_tS);
+plot(sigma_pG);
+
 %plot(EC_S);
 %plot(EC_G);
 %plot(ECGECS);
 
+legend('show')
